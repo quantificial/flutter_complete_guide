b671db78aa1de5dbcf24df9066b45efea1df0fb7
Johnson Fu <johnson.fu@gmail.com>
2023-03-18 16:49:42 +0800

    add buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  var questions = ['aaaa', 'bbbbb'];
+
+  void changeTitle() {
+    print('dsadsadsa');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       //home: Text('Hello!'),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Text('data'),
-      ),
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(
+            children: [
+              Text(questions[0]),
+              ElevatedButton(onPressed: changeTitle, child: Text('Answer 1')),
+              ElevatedButton(
+                  onPressed: () => {print('press answer 2')},
+                  child: Text('Answer 2')),
+              ElevatedButton(
+                  onPressed: () {
+                    print('pressed answer 3');
+                  },
+                  child: Text('Answer 3')),
+            ],
+          )),
     );
   }
 }
